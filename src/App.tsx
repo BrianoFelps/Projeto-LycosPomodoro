e00bd716ff2fdc2d82d4298c9b3ad115b10a2c9b
BrianoFelps <briansticknodesbr@gmail.com>
2024-08-08 21:05:24 -0300

    Alterações majoritárias para um estilo harmonizado e começo de responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fontsource/montserrat'
+import Carousel from 'react-bootstrap/Carousel';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LycosCompleteLogo from './assets/Svg/LycosCompleteLogo';
 
@@ -13,7 +14,41 @@ function App() {
           <Route></Route>
         </Routes>
       </BrowserRouter> */}
-      <LycosCompleteLogo></LycosCompleteLogo>
+      <div id='ContainerBg'></div>
+      <div id='ContainerBg2'></div>
+      <div id='HeroBannerContainer'>
+        <div id='ContainerTopoLogo'>
+          <LycosCompleteLogo></LycosCompleteLogo>
+        </div>
+        <div id='CallToActionBanner'>
+          <Carousel>
+            <Carousel.Item interval={50000000}>
+              <div className="d-flex justify-content-center align-items-center text-center">
+                  <h1 className='display-1 fw-bolder'>Atualize seu potencial!</h1>
+              </div>
+              <Carousel.Caption>
+                <h3 className='lead'>Lycos pomotimer é um site que fomentará seu foco com o método pomodoro! Realize seus objetivos em potencial, e alcance a forma mais sublime de si. Bons estudos!</h3>
+                <div className='ContainerBotoes'>
+                  <button className='btn btn-outline-light'>Cadastrar</button>
+                  <button className='btn btn-success loginbtn'>Entrar</button>
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={50000000}>
+              <div className="d-flex justify-content-center align-items-center text-center">
+                  <h1 className='display-1  fw-bolder'>Atualize seu potencial!</h1>
+              </div>
+              <Carousel.Caption>
+                <h3 className='lead'>bottom text</h3>
+                <div className='ContainerBotoes'>
+                  <button className='btn btn-outline-light'>a</button>
+                  <button className='btn btn-success loginbtn'>b</button>
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
     </>
   )
 }
